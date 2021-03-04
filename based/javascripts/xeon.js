691b9c77e871d5c0de1745de8d6993207e5c7175
Jefare <45242774+govae@users.noreply.github.com>
2021-03-05 00:50:12 +0800

    Update xeon.js

--- based/javascripts/xeon.js
+++ based/javascripts/xeon.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?0f5ac128b1a0af2d229d008731b6c40b";
+  hm.src = "https://hm.baidu.com/hm.js?0f5ac128b1a0af2d229d008731b6c409";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
